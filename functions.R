69ae712e589100b660ffa85b808a92b22b1c50fc
tsmahajcsiks <tsmahajcsiks@avaya.com>
2019-05-09 13:51:37 +0200

    SAVE PROGRESS

--- functions.R
+++ functions.R
@@ -138,47 +138,52 @@ bootstrap <- function(x, fun, B = 599, trim = TRUE, tr = 0.2, seed = TRUE, perce
   print(results)
 }
 
-bootstrap2 <- function(x, fun, mode = "sample", B = 599, trimming = TRUE, tr = 0.2, seed = TRUE, percentile = TRUE) {
+bootstrap2 <- function(x, fun, mode = "estimate", B = 599, trimming = TRUE, tr = 0.2, seed = TRUE, percentile = TRUE) {
   if(seed) {set.seed(1745)}
-  if (is.list(x)){
-    catcher <- matrix(ncol = length(x), nrow = B)
-    for (v in seq(1, length(x))) {
-    x_selected <- x[[v]]
-    place_holder <- c()
-      for (i in seq(1,B)){
-        if (trimming) {
-          boot_sample <- sample(x_selected, replace = TRUE)
-          boot_sample <- trim(boot_sample, tr)
-          est <- do.call(fun, list(boot_sample))
-          place_holder[i] <- est
-        } else {
-          boot_sample <- sample(x_selected, replace = TRUE)
-          est <- do.call(fun, list(boot_sample))
-          place_holder[i] <- est
-          }
-      } 
-    catcher[,v] <- place_holder
-    }
-    catcher
-  } else {
-  catcher <- numeric()
-  for (i in seq(1,B)) {
-    if (trimming) {
-      boot_sample <- sample(x, replace = TRUE)
-      boot_sample <- trim(boot_sample, tr)
-      est <- do.call(fun, list(boot_sample))
-      catcher[i] <- est
+  if (mode == "estimate") {
+    if (is.list(x)){
+      catcher <- matrix(ncol = length(x), nrow = B)
+      for (v in seq(1, length(x))) {
+      x_selected <- x[[v]]
+      place_holder <- c()
+        for (i in seq(1,B)){
+          if (trimming) {
+            boot_sample <- sample(x_selected, replace = TRUE)
+            boot_sample <- trim(boot_sample, tr)
+            est <- do.call(fun, list(boot_sample))
+            place_holder[i] <- est
+          } else {
+            boot_sample <- sample(x_selected, replace = TRUE)
+            est <- do.call(fun, list(boot_sample))
+            place_holder[i] <- est
+            }
+        } 
+      catcher[,v] <- place_holder
+      }
       catcher
     } else {
-      boot_sample <- sample(x, replace = TRUE)
-      est <- do.call(fun, list(boot_sample))
-      catcher[i] <- est
-      catcher
+    catcher <- numeric()
+    for (i in seq(1,B)) {
+      if (trimming) {
+        boot_sample <- sample(x, replace = TRUE)
+        boot_sample <- trim(boot_sample, tr)
+        est <- do.call(fun, list(boot_sample))
+        catcher[i] <- est
+        catcher
+      } else {
+        boot_sample <- sample(x, replace = TRUE)
+        est <- do.call(fun, list(boot_sample))
+        catcher[i] <- est
+        catcher
+        }
       }
     }
+  } else if (mode == "sample") {
+    x_selected <- x[[v]]
+    place_holder <- c()
+    for (v in seq(1, length(x))) {}
+    } else {}
   }
-}
-
 
 wincor <- function(x, y, tr = .2) {
   g <- floor(length(x) * tr)
