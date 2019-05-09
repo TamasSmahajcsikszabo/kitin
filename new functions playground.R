65a6df9da40069ca883da202db0e564a2d773352
tsmahajcsiks <tsmahajcsiks@avaya.com>
2019-05-09 10:51:02 +0200

    bootstrap2 fixed

--- new functions playground.R
+++ new functions playground.R
@@ -1,5 +1,6 @@
 a <- c(17,15,12,6,2,5,4,9,8,7,5,6,7)
 b <- round(a+ rnorm(length(a))*4,0)
+c <- a
 
-result4 <- bootstrap2(list(a,b), "trimmed_mean")
+result4 <- bootstrap2(list(a,b,c), "trimmed_mean")
 assign("a",c())
