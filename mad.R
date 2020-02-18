efb455cd2ebb7e476a7c5ca5123a2994c247b919
tamas <tamas@localhost.localdomain>
2020-02-18 21:42:14 +0100

    visuals for MAD outliers

--- mad.R
+++ mad.R
@@ -37,10 +37,6 @@ MAD <- function(x, verbose = TRUE) {
 
 x <- c(34, 49, 49, 44, 66, 48, 49, 39, 54, 57, 39, 65, 43, 43, 44, 42, 71, 40, 41, 38, 42, 77, 40, 38, 43, 42, 36, 55, 57, 57, 41, 66, 69, 38, 49, 51, 45, 141, 133, 76, 44, 40, 56, 50, 75, 44, 181, 45, 61, 15, 23, 42, 61, 146, 144, 89, 71, 83, 49, 43, 68, 57, 60, 56, 63, 136, 49, 57, 64, 43, 71, 38, 74, 84, 75, 64, 48)
 
-plot(x)
-MAD(x)
-
-
 MAD_plot <- function(x) {
   estimates <- MAD(x)
   plot_data <- data.frame(x)
@@ -51,12 +47,28 @@ MAD_plot <- function(x) {
   plot_data[plot_data$outlier == TRUE, 3] <- plot_data[plot_data$outlier == TRUE, 1]
   plot_data[plot_data$outlier == FALSE, 3] <- NA
 
+  boundaries <- t(data.frame(
+    median(x, na.rm = TRUE) - estimates[[2]],
+    median(x, na.rm = TRUE) + estimates[[2]],
+    median(x, na.rm = TRUE) - 2 * estimates[[2]],
+    median(x, na.rm = TRUE) + 2 * estimates[[2]]
+  ))
+  rownames(boundaries) <- c("-1s", "+1s", "-2s", "+2s")
+
   ggplot(plot_data) +
-    geom_point(aes(index, outlier), size = 5, color = "coral") +
-    geom_point(aes(index, x), size = 2, color = "cornflowerblue") +
+    geom_hline(aes(yintercept = median(x, na.rm = TRUE)), linetype = "longdash", size = .5, alpha = 1/2) +
+    geom_hline(aes(yintercept = boundaries[1, ][[1]]), linetype = "twodash", size = .5, alpha = 1/2) +
+    geom_hline(aes(yintercept = boundaries[2, ][[1]]), linetype = "twodash", size = .5, alpha = 1/2) +
+    geom_hline(aes(yintercept = boundaries[3, ][[1]]), linetype = "dotted", size = .5, alpha = 1/2) +
+    geom_hline(aes(yintercept = boundaries[4, ][[1]]), linetype = "dotted", size = .5, alpha = 1/2) +
+    geom_point(aes(index, outlier), size = 6, color = "coral") +
+    geom_point(aes(index, x), size = 3, color = "cornflowerblue") +
     theme_light() +
     labs(
       x = "Index of observation",
-      y = "Value of observation"
-    )
+      y = "Value of observation") +
+    geom_text(aes(index, outlier, label = x))
+
+
 }
+MAD_plot(x)
